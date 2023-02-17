2f86a65508ba552bfb49c78ff85baa9fa337b237
adindozo <adin.dozo1@outlook.com>
2023-02-17 15:43:33 +0100

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# react-chess
-A chess game built using React.js.
-The game features a user-friendly interface, the ability to play against a friend, and a detailed game history. 
-Perfect for chess enthusiasts of all skill levels.
--- src/pieces/king.js
+++
@@ -1,9 +0,0 @@
-import { getKingMovement } from '../utilities/movement';
-import Piece from './piece'
-class King extends Piece{
-    getPossibleMoves(board){
-        return getKingMovement(board, this.position, this.color)
-    }
-}
-
-export default King;
